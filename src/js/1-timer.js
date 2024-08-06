6866e7bd6673d6fc99ab88c1de4a3f0a4479e621
Your Name <112875685+Tormoond@users.noreply.github.com>
2024-08-06 21:13:49 +0200

    ...)
    Видалення файлів по Js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+
 
 const startButton = document.querySelector('[data-start]');
 const dateTimePicker = document.getElementById('datetime-picker');
