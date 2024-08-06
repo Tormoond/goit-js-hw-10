ec7d9d8011b740cfae51686b216f4a296c87d493
Tormoond <112875685+Tormoond@users.noreply.github.com>
2024-08-06 19:14:09 +0000

    Deploying to gh-pages from @ Tormoond/goit-js-hw-10@6866e7bd6673d6fc99ab88c1de4a3f0a4479e621 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.delay.value),i=t.target.state.value;new Promise((e,o)=>{setTimeout(()=>{i==="fulfilled"?e(s):o(s)},s)}).then(e=>{iziToast.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{iziToast.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=commonHelpers2.js.map
